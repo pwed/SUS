51be8ca109ec6e232fbab4b4bad9c44fd73cc252
Fred Stoddart <freddiestoddart000@gmail.com>
2024-01-17 22:36:12 +1100

    init frontend and lambda

--- cdk/sus.ts
+++ cdk/sus.ts
@@ -1,22 +1,36 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import { App, RemovalPolicy, Stack, StackProps, aws_apigatewayv2 as apigw, aws_s3 as s3 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as go from '@aws-cdk/aws-lambda-go-alpha';
+import { join } from 'path';
 
-class SusStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+class SusStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, "UploadBucket", {
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SusQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const api = new apigw.HttpApi(this, "Api", {})
+
+    const uploadFunction = new go.GoFunction(this, "UploadFunction", {
+      entry: join(__dirname, "..", "lambda", "upload"),
+      environment: {
+        "UPLOAD_BUCKET": bucket.bucketName,
+      }
+    })
   }
 }
 
-const app = new cdk.App();
+const app = new App();
 new SusStack(app, 'SusStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
