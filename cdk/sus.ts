b83f237cb67d9bc15f8a575d46fd4ce159fb2ab8
Fred Stoddart <freddiestoddart000@gmail.com>
2024-01-17 16:41:09 +1100

    move cdk into own folder

--- cdk/sus.ts
+++ cdk/sus.ts
@@ -1,7 +1,20 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { SusStack } from '../lib/sus-stack';
+import { Construct } from 'constructs';
+
+class SusStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'SusQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
 
 const app = new cdk.App();
 new SusStack(app, 'SusStack', {
